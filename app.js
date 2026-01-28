e18e7ce80f5b9fd16f2c484ac755b74a57a8aaf1
karimiarrison <karimiarrison@gmail.com>
2026-01-28 18:43:20 +0300

    88% drawer

--- app.js
+++ app.js
@@ -4015,7 +4015,7 @@ function openDrawer() {
 
 function closeDrawer() {
   if (window.innerWidth < 1024) {
-    drawer.style.transform = "translateY(71%)";
+    drawer.style.transform = "translateY(80%)";
   } else {
     drawer.style.transform = "translateY(-40%)";
   }
